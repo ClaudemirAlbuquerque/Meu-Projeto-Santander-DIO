b0c19150cbaa92e8030dd812f944be899f1fa8e7
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-19 20:21:45 -0300

    Modulo Javascript Modulos

---
+++ Projeto-javascript/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Claudemir", 35));
+console.log(mostraCidade("Claudemir", "Cabo de Santo Agostinho"));
+console.log(mostraHobby("Claudemir", "Assitir"));
