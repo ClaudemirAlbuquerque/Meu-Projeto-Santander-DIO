5508140564943a9143b72430c84ed93148cf94b3
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-21 22:07:28 -0300

    atualização do typescript

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,1 +1,48 @@
 console.log('Typescript');
+
+/*function soma(a: number, b: number) {
+    return a + b;
+}*/
+
+/*
+//types
+// interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terreste' | 'aquatico';
+    domestico: boolean;
+}
+interface Ifelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = Ifelino | ICanino;
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: 'terreste',
+}*/
+
+/*
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    console.log('Digitei');
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})*/
+
+
+//Generic types
+
+function adicionaApendiceALista<T>(array: T[], valor: T) {
+    return array.map(() => valor);
+}
+
+adicionaApendiceALista([1, 2, 3], 2);
