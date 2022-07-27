b5ad545b7c21f7917e84e3e440be69058ed2d1cf
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-26 22:18:52 -0300

    Aulas de Angular continuação

--- Angular/course-manager/src/app/courses/course-list.component.ts
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component ({
     selector: 'app-course-list',
@@ -10,28 +11,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private CourseService: CourseService) { }
+
   ngOnInit(): void {
-      this.courses = [
-          {
-              id: 1,
-              name: 'Angular: Forms',
-              imageUrl: '/assets/images/forms.png',
-              price: 99.99,
-              code: 'XPS-8796',
-              duration: 120,
-              rating: 4.5,
-              releaseDate: 'Julho, 26, 2022'
-          },
-          {
-              id: 2,
-              name: 'Angular: HTTP',
-              imageUrl: 'assets/images/http.png',
-              price: 45.99,
-              code: 'LKL-8796',
-              duration: 80,
-              rating: 4,
-              releaseDate: 'Julho, 28, 2022'
-          }
-      ]
+      this.courses = this.CourseService.retrieveAll();
+
   }
 }
