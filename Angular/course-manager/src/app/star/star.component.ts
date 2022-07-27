557c2d6b85130ceaf1521929f314c05effa0eb6c
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-26 21:46:55 -0300

    Aulas de Angular continua.

--- Angular/course-manager/src/app/star/star.component.ts
+++ Angular/course-manager/src/app/star/star.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
   selector: 'app-star',
-  templateUrl: './star.component.html'
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
 })
 
 export class StarComponent implements OnChanges {
@@ -15,5 +16,4 @@ export class StarComponent implements OnChanges {
     ngOnChanges(): void {
         this.starWidth = this.rating * 94 / 5;
     }
-
 }
