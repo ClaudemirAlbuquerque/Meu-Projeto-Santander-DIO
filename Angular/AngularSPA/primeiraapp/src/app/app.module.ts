29fe73df11fcda8a46a5a5fe42ac3b8c645e806b
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-28 17:43:47 -0300

    Aulas de Angular SPA componentes

--- Angular/AngularSPA/primeiraapp/src/app/app.module.ts
+++ Angular/AngularSPA/primeiraapp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
