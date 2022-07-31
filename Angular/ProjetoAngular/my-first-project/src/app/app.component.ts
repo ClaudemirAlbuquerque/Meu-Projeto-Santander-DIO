b4108159ca2b65102cfffa5585ec852274d42942
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-07-31 18:16:43 -0300

    Continuação das Aulas de Angular

--- Angular/ProjetoAngular/my-first-project/src/app/app.component.ts
+++ Angular/ProjetoAngular/my-first-project/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Meu-Primeiro-Projeto';
+export class AppComponent implements OnInit {
+    count = 0;
+    name = "Claudemir Albuquerque";
+    text = "";
+
+    pessoas = [
+      {
+          nome: "Claudemir",
+          sobrenome: "Albuquerque"
+      },
+      {
+          nome: "Natalie",
+          sobrenome: "Albuquerque"
+      },
+      {
+        nome: "Natália",
+        sobrenome: "Albuquerque"
+      },
+      {
+          nome: "Bela",
+          sobrenome: "Albuquerque"
+      }
+    ];
+
+    construtor(){
+
+    }
+
+    ngOnInit() {
+      let interval = setInterval(() => {
+        this.count++;
+        if(this.count === 10) {
+          clearInterval(interval)
+        }
+      },1000)
+    }
+
+    clicou(nome: string): void {
+        console.log('Clicou em mim', nome);
+    }
 }
--- Angular/ProjetoAngular/my-first-project/src/app/app.module.ts
+++ Angular/ProjetoAngular/my-first-project/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
+import { PeopleService } from './shared/services/people.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    FormsModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
