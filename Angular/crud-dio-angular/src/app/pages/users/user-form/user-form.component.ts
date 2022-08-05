48da39258a87dd84c8c3b03ac70bf12008333c7d
ClaudemirAlbuquerque <104981601+ClaudemirAlbuquerque@users.noreply.github.com>
2022-08-04 23:54:44 -0300

    Aulas de Assincronos Angular

--- Angular/crud-dio-angular/src/app/pages/users/user-form/user-form.component.ts
+++ Angular/crud-dio-angular/src/app/pages/users/user-form/user-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +10,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  UserForm: FormGroup;
+  users: Array<User> = [];
+  userId: any = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+    private userService: UserService,
+    private actRoute: ActivatedRoute, private router: Router) {
+    this.UserForm = this.fb.group({
+      id: 0,
+      nome: '',
+      sobrenome: '',
+      idade: '',
+      profissao: '',
+    })
   }
 
+  ngOnInit(): void {
+    this.actRoute.paramMap.subscribe(params => {
+      this.userId = params.get('id');
+      console.log(this.userId);
+      if(this.userId !== null) {
+        this.userService.getUser(this.userId).subscribe(result => {
+          this.UserForm.patchValue({
+            id: result[0].id,
+            nome: result[0].nome,
+            sobrenome: result[0].sobrenome,
+            idade: result[0].idade,
+            profissao: result[0].profissao,
+          })
+        })
+      }
+    })
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    })
+  }
+
+  createUser() {
+    this.UserForm.get("id")?.patchValue(this.users.length + 1);
+    this.userService.postUser(this.UserForm.value).subscribe(result => {
+      console.log(`Usuário $(result.nome) $(result.sobrenome) foi cadastrado com sucesso!`)
+    }, (err) => {
+
+    }, () => {
+      this.router.navigate(['/']);
+    })
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.userId, this.UserForm.value).subscribe(result => {
+      console.log('Usuário Atualizado', result);
+    }, (err) => {
+
+    }, () => {
+      this.router.navigate(['/']);
+    })
+  }
+
+  actionButton() {
+    if(this.userId !== null) {
+      this.updateUser()
+    }else {
+      this.createUser()
+    }
+  }
 }
--- Angular/crud-dio-angular/src/app/services/user.service.ts
+++ Angular/crud-dio-angular/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -8,6 +8,11 @@ import { User } from '../models/user';
 })
 export class UserService {
   apiUrl = 'https://sheet.best/api/sheets/e9dbeefa-cd0c-450e-ac43-b456313226e3';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,4 +23,23 @@ export class UserService {
     return this.httpClient.get<User[]>(this.apiUrl);
   }
 
+  // Salva usuário no banco POSTE
+  postUser(user: User):Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions)
+  }
+
+  // Excluir o usuário do banco DELETE
+  deleteUser(id: number):Observable<User> {
+    return this.httpClient.delete<User>(`${this.apiUrl}/id/${id}`)
+  }
+
+  // Edita usuário UPDATE
+  updateUser(id: string, user: User):Observable<User> {
+    return this.httpClient.put<User>(`${this.apiUrl}/id/${id}`, user, this.httpOptions)
+  }
+
+  // Lista usuário unico
+  getUser(id: string):Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.apiUrl}/id/${id}`)
+  }
 }
